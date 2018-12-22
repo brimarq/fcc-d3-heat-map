53c123a5bd87bfe153b532734ee3d568b7879f7b
Brian Marquis <rbmarquis.dev@gmail.com>
2018-12-21 21:29:37 -0600

    set and test dataUrl

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const dataUrl = "" 
+const dataUrl = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
 let json, req = new XMLHttpRequest();
 
 /** Send http req */
@@ -6,6 +6,6 @@ req.open("GET", dataUrl ,true);
 req.send();
 req.onload = function() {
   json = JSON.parse(req.responseText);
-  d3.select("main div#svg-container").text(json);
+  d3.select("main div#svg-container").text(JSON.stringify(json));
 };
 
